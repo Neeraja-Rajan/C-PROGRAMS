71328201eed580c90baed8c91deeade800777a36
Neeraja-Rajan <65912613+Neeraja-Rajan@users.noreply.github.com>
2021-09-30 19:47:58 +0530

    Create factor.cpp

---
+++ factor.cpp
@@ -0,0 +1,14 @@
+  #include <iostream>
+    using namespace std;
+    //main Program
+    int main()
+    {
+        int num;
+        cout << “Enter a positive number: “;
+        //user input
+        cin >> num;
+        cout << “Factors of “ << num << ” are: “ << endl;  
+        //finding and printing factors
+     
+        return 0;
+    }
