fd642631966209063d9767e5066b29b4a2fccf9d
Neeraja-Rajan <65912613+Neeraja-Rajan@users.noreply.github.com>
2021-09-30 19:47:22 +0530

    Create power.cpp

---
+++ power.cpp
@@ -0,0 +1,18 @@
+ #include <iomanip>
+    #include <iostream>
+    #include <math.h>
+    using namespace std;
+    //main program
+    int main() 
+    {
+        double exp, base;
+        cout<<“Enter base: “;
+        //user input 1
+        cin>>base;
+        cout<<“Enter Exponent: “;
+        //user input 2
+        cin>>exp;
+        //calculating power using function
+        
+        return 0;
+    }
